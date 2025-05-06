892d1e6227cef4427e59b5802fb4fd16d43639b6
Shitanshu Kumar <shitanshukumar639@gmail.com>
2025-05-06 18:54:16 +0530

    Add Checkout component

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -4,6 +4,7 @@ import Home from "@/pages/home/Home";
 import Login from "@/components/Login";
 import Register from "@/components/Register";
 import Cart from "@/pages/cart/Cart";
+import Checkout from "@/pages/checkout/Checkout";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
       { path: "about", element: <h1>About</h1> },
       { path: "contact", element: <h1>Contact</h1> },
       { path: "/cart", element: <Cart /> },
+      { path: "/checkout", element: <Checkout /> },
     ],
   },
   {
