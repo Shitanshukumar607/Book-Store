1af5d340adcbb2302b64f41b9b9bdbc1e6d38062
Shitanshu Kumar <shitanshukumar639@gmail.com>
2025-05-24 00:05:50 +0530

    add: loading

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -2,8 +2,9 @@ import { useAuth } from "@/context/AuthContext";
 import { Navigate } from "react-router";
 
 const PrivateRoute = ({ children }) => {
-  const { currentUser: user } = useAuth();
+  const { currentUser: user, isLoading } = useAuth();
 
+  if (isLoading) return <div>Loading...</div>;
   if (user) return children;
 
   return <Navigate to="/login" replace></Navigate>;
